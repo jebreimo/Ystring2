a61385d71b3a3014662d1c07658fc731af167f67
Jan Erik Breimo <jebreimo@gmail.com>
2020-06-25 23:36:47 +0200

    Add test of Utf16Decoder.

--- tests/YstringTest/test_Utf16Decoder.cpp
+++ tests/YstringTest/test_Utf16Decoder.cpp
@@ -12,5 +12,10 @@
 
 TEST_CASE("Test UTF-16 decoder")
 {
-
+    std::string s("\x00A\x00B\x00C\x00D\x00E\x00F\x00G", 14);
+    Ystring::Utf16Decoder<Ystring::IsLittleEndian> decoder;
+    std::vector<char32_t> u(7);
+    auto [m, n] = decoder.decode(s.data(), s.size(), u.data(), u.size());
+    REQUIRE(m == 14);
+    REQUIRE(n == 7);
 }
