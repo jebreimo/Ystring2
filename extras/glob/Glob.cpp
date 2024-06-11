b05c24babfef130ccaf86248994d62ba8ead2f98
Jan Erik Breimo <jebreimo@gmail.com>
2024-06-11 21:44:11 +0200

    The test command for globbing works, more or less.

--- extras/glob/Glob.cpp
+++ extras/glob/Glob.cpp
@@ -9,130 +9,270 @@
 #include <iostream>
 #include <Ystring/Algorithms.hpp>
 #include <Ystring/GlobMatcher.hpp>
+#include <Ystring/PathMatcher.hpp>
+#include <utility>
 
-struct Expression
+class PathIteratorBase
 {
-    std::filesystem::path base_path;
+public:
+    virtual ~PathIteratorBase() = default;
+
+    virtual void set_base_path(std::filesystem::path base_path) = 0;
+
+    virtual bool next() = 0;
+
+    bool next_directory()
+    {
+        while (next())
+        {
+            if (std::filesystem::is_directory(path()))
+                return true;
+        }
+        return false;
+    }
+
+    [[nodiscard]]
+    virtual std::filesystem::path path() const = 0;
 };
 
-struct Stars {};
-
-using GlobPathPart = std::variant<std::filesystem::path, Stars, ystring::GlobMatcher>;
-using GlobPath = std::vector<GlobPathPart>;
-
-std::pair<std::filesystem::path, GlobPath>
-parse_glob_path(std::filesystem::path path)
+class SinglePathIterator : public PathIteratorBase
 {
-    GlobPath result;
-    while (true)
+public:
+    SinglePathIterator(std::filesystem::path sub_path, bool has_next)
+        : sub_path_(std::move(sub_path)),
+          has_next_(has_next)
+    {}
+
+    void set_base_path(std::filesystem::path base_path) override
     {
-        auto filename = path.filename();
-        auto filename_str = filename.u8string();
-        auto filename_view = ystring::to_string_view(filename_str);
-        if (filename == u8"**")
-            result.emplace_back(Stars());
-        else if (ystring::is_glob_pattern(filename_view))
-            result.emplace_back(ystring::GlobMatcher(filename_view));
-        else if (!filename.empty())
-            result.emplace_back(filename);
-        auto parent_path = path.parent_path();
-        if (parent_path == path)
+        base_path_ = std::move(base_path);
+        has_next_ = true;
+    }
+
+    bool next() override
+    {
+        if (!has_next_)
+            return false;
+
+        has_next_ = false;
+        return exists(base_path_ / sub_path_);
+    }
+
+    [[nodiscard]]
+    std::filesystem::path path() const override
+    {
+        return base_path_ / sub_path_;
+    }
+
+public:
+    std::filesystem::path base_path_;
+    std::filesystem::path sub_path_;
+    bool has_next_ = true;
+};
+
+class GlobIterator : public PathIteratorBase
+{
+public:
+    explicit GlobIterator(ystring::GlobMatcher matcher)
+        : matcher_(std::move(matcher))
+    {}
+
+    bool next() override
+    {
+        while (it_ != end_)
         {
-            if (!parent_path.empty())
-                result.emplace_back(parent_path);
-            break;
+            auto filename = it_->path().filename().u8string();
+            if (matcher_.match(ystring::to_string_view(filename)))
+            {
+                current_path_ = it_->path();
+                ++it_;
+                return true;
+            }
+            ++it_;
         }
-        path = parent_path;
+        return false;
     }
 
-    std::filesystem::path parent_dir;
-    auto it = result.rbegin();
-    while (it != result.rend())
+    void set_base_path(std::filesystem::path base_path) override
     {
-        if (!std::holds_alternative<std::filesystem::path>(*it))
-            break;
-
-        if (parent_dir.empty())
-            parent_dir = std::get<std::filesystem::path>(*it);
-        else
-            parent_dir /= std::get<std::filesystem::path>(*it);
-        ++it;
+        base_path_ = std::move(base_path);
+        it_ = std::filesystem::directory_iterator(base_path_);
+        end_ = end(it_);
     }
 
-    result.erase(it.base(), result.end());
-    std::reverse(result.begin(), result.end());
-    return {parent_dir, result};
+    [[nodiscard]]
+    std::filesystem::path path() const override
+    {
+        return current_path_;
+    }
+private:
+    std::filesystem::directory_iterator it_;
+    std::filesystem::directory_iterator end_;
+    std::filesystem::path base_path_;
+    std::filesystem::path current_path_;
+    ystring::GlobMatcher matcher_;
+};
+
+class DoubleStarIterator : public PathIteratorBase
+{
+public:
+    explicit DoubleStarIterator(ystring::PathMatcher matcher)
+        : matcher_(std::move(matcher))
+    {}
+
+    void set_base_path(std::filesystem::path base_path) override
+    {
+        base_path_ = std::move(base_path);
+        it_ = std::filesystem::recursive_directory_iterator(base_path_);
+        end_ = end(it_);
+    }
+
+    bool next() override
+    {
+        while (it_ != end_)
+        {
+            if (matcher_.match(it_->path()))
+            {
+                current_path_ = it_->path();
+                ++it_;
+                return true;
+            }
+            ++it_;
+        }
+        return false;
+    }
+
+    [[nodiscard]]
+    std::filesystem::path path() const override
+    {
+        return current_path_;
+    }
+private:
+    std::filesystem::recursive_directory_iterator it_;
+    std::filesystem::recursive_directory_iterator end_;
+    std::filesystem::path base_path_;
+    std::filesystem::path current_path_;
+    ystring::PathMatcher matcher_;
+};
+
+std::filesystem::path
+make_path(const std::filesystem::path::const_iterator& begin,
+          const std::filesystem::path::const_iterator& end,
+          std::filesystem::path prefix = {})
+{
+    std::filesystem::path path = std::move(prefix);
+    for (auto it = begin; it != end; ++it)
+        path /= *it;
+    return path;
 }
 
-std::ostream& operator<<(std::ostream& os, const GlobPathPart& part)
+std::vector<std::unique_ptr<PathIteratorBase>>
+parse_glob_path(const std::filesystem::path& path)
 {
-    struct Visitor
+    std::vector<std::unique_ptr<PathIteratorBase>> result;
+    std::filesystem::path plain_path;
+
+    for (auto it = path.begin(), end = path.end(); it != end; ++it)
     {
-        std::ostream& os;
-
-        void operator()(const std::filesystem::path& path) const
+        auto name = it->u8string();
+        if (name == u8"**")
         {
-            os << path;
+            if (!plain_path.empty())
+            {
+                result.emplace_back(std::make_unique<SinglePathIterator>(std::move(plain_path), result.empty()));
+                plain_path = std::filesystem::path();
+            }
+
+            result.emplace_back(std::make_unique<DoubleStarIterator>(
+                ystring::PathMatcher(make_path(++it, end, u8"**"))));
+            break;
         }
 
-        void operator()(const Stars&) const
+        if (ystring::is_glob_pattern(ystring::to_string_view(name)))
         {
-            os << "**";
-        }
+            if (!plain_path.empty())
+            {
+                result.emplace_back(std::make_unique<SinglePathIterator>(std::move(plain_path),
+                                                                         result.empty()));
+                plain_path = std::filesystem::path();
+            }
 
-        void operator()(const ystring::GlobMatcher& matcher) const
-        {
-            os << matcher;
+            result.emplace_back(std::make_unique<GlobIterator>(
+                ystring::GlobMatcher(ystring::to_string_view(name))));
         }
-    };
-    std::visit(Visitor{os}, part);
-    return os;
+        else
+        {
+            plain_path /= *it;
+        }
+    }
+
+    if (!plain_path.empty())
+    {
+        result.emplace_back(std::make_unique<SinglePathIterator>(std::move(plain_path),
+                                                                 result.empty()));
+    }
+
+    return result;
 }
 
 class GlobPathIterator
 {
 public:
     explicit GlobPathIterator(const std::filesystem::path& glob_path)
-    {
-        auto [parent_dir, parts] = parse_glob_path(glob_path);
-        for (const auto& part : parts)
-            std::cout << part << '\n';
-
-    }
+        : iterators_(parse_glob_path(glob_path))
+    {}
 
     bool next()
     {
-        //while (m_it != std::filesystem::directory_iterator())
-        //{
-        //    if (m_it->is_directory())
-        //    {
-        //        m_stack.push_back(m_it);
-        //        m_it = std::filesystem::directory_iterator(*m_it);
-        //    }
-        //    else
-        //    {
-        //        m_current = *m_it;
-        //        ++m_it;
-        //        return true;
-        //    }
-        //}
-        //
-        //if (!m_stack.empty())
-        //{
-        //    m_it = m_stack.back();
-        //    m_stack.pop_back();
-        //    ++m_it;
-        //    return next();
-        //}
+        if (iterators_.empty())
+            return false;
+        if (iterators_.back()->next())
+            return true;
 
+        auto last = --iterators_.cend();
+        auto it = last;
+        while (true)
+        {
+            if (!find_prev_with_next(it))
+                return false;
+
+            std::filesystem::path path = (*it)->path();
+            for (++it; it != last; ++it)
+            {
+                (*it)->set_base_path(std::move(path));
+                if (!(*it)->next_directory())
+                    break;
+                path = (*it)->path();
+            }
+            if (it == last)
+            {
+                (*it)->set_base_path(std::move(path));
+                if ((*it)->next())
+                    return true;
+            }
+        }
+    }
+
+    [[nodiscard]]
+    std::filesystem::path path() const
+    {
+        return iterators_.back()->path();
+    }
+private:
+    using Container = std::vector<std::unique_ptr<PathIteratorBase>>;
+
+    bool find_prev_with_next(Container::const_iterator& it) const
+    {
+        while (it != iterators_.cbegin())
+        {
+            --it;
+            if ((*it)->next_directory())
+                return true;
+        }
         return false;
     }
 
-    //const std::filesystem::directory_entry& operator*() const
-    //{
-    //    return it_.operator*();
-    //}
-
+    std::vector<std::unique_ptr<PathIteratorBase>> iterators_;
 };
 
 int main(int argc, char* argv[])
@@ -143,15 +283,10 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    if (ystring::is_glob_pattern(argv[1]))
-    {
-        GlobPathIterator it(argv[1]);
-        //while (it.next())
-        //    std::cout << *it << '\n';
-    }
-    //for (const auto entry : ystring::split(argv[1], U"/"))
-    //    std::cout << entry << '\n';
-    GlobPathIterator matcher(argv[1]);
+    std::clog << "Glob path: " << argv[1] << '\n';
+    GlobPathIterator it(argv[1]);
+    while (it.next())
+        std::clog << it.path() << '\n';
 
     return 0;
 }
